f091fdad62462641fd396b14fed6fb606f4393c6
bielangelo011x-cyber <bielangelo011x@gmail.com>
2026-09-03 08:41:00 -0300

    adição de filmes
    foi adicionado quantidade x de filmes

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# MeuSiteProfDavi
- 
+# Meu_Site_Prof_Davi
+ Esse é um site pessoal que listará meus Filmes e Séries Preferidos.
---
+++ filmes.js
@@ -0,0 +1,221 @@
+// filmes.js
+
+export const filmes = [
+    {
+        id: "interestelar",
+        titulo: "Interestelar",
+        imagem: "Imagens/interestelar.jpg",
+        descricao: "Um grupo de astronautas viaja através de um buraco de minhoca em busca de um novo lar para a humanidade.",
+        trailer: "i6avfCqKcQo?si=c67bqEGle7cgphln",
+
+        // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
+
+        duracao: "2h 10 min",
+        genero: "ficcção",
+        classificacao: "+16",
+    },
+
+    {
+        id: "Batman",
+        titulo: "Batman",
+        imagem: "Imagens/batman.png",
+        descricao: "Em Batman: O Cavaleiro das Trevas, a paz é ameaçada pelo Coringa, um anarquista que instaura o caos.",
+        trailer: "mqqft2x_Aa4?si=kC9DGfgVmgNtKh0o",
+
+        // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
+
+        duracao: "2h10m",
+        genero: "ação",
+        classificacao: "+14",
+    },
+
+    {
+        id: "Ong Bak",
+        titulo: "Ong Bak",
+        imagem: "Imagens/ongbak.jpg",
+        descricao: "Ong-Bak: Guerreiro Sagrado, acompanha Ting (Tony Jaa), um jovem guerreiro de uma pacata vila tailandesa.",
+        trailer: "gGwF9mtDb5c",
+
+        // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
+
+        duracao: "2hrs",
+        genero: "ação",
+        classificacao: "+12",
+    },
+
+    {
+        id: "Kung-Fusão",
+        titulo: "Kung-Fusão",
+        imagem: "Imagens/kungfusao.jpg",
+        descricao: "A história acompanha Sing, um malandro trapalhão que tenta entrar para a temida Gangue do Machado.",
+        trailer: "KcL5naEK8HI",
+        
+        // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
+
+        duracao: "2h05min",
+        genero: "ação",
+        classificacao: "+12",
+    },
+
+    {
+        id: "Homem Aranha",
+        titulo: "Homem Aranha",
+        imagem: "Imagens/homemaranha.png",
+        descricao: "O jovem Peter Parker que, após ser picado por uma aranha, ganha superpoderes como força, agilidade e a capacidade de escalar paredes.",
+        trailer: "PlulyWs1kS4",
+        
+        // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
+
+        duracao: "2h24min",
+        genero: "ação",
+        classificacao: "+16",
+    },
+
+    {
+        id: "Capitão América: Guerra Civil",
+        titulo: "Capitão América: Guerra Civil",
+        imagem: "Imagens/capitaoamerica.png",
+        descricao: "Após danos colaterais em missões dos Vingadores, a ONU propõe o Tratado de Sokovia, um acordo para regulamentar e controlar a equipe.",
+        trailer: "FRLYZn5PGuA",
+        
+        // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
+
+        duracao: "2h28min",
+        genero: "ação",
+        classificacao: "+12",
+    },
+
+    {
+        id: "Capitão América: Guerra Civil",
+        titulo: "Capitão América: Guerra Civil",
+        imagem: "Imagens/capitaoamerica.png",
+        descricao: "Após danos colaterais em missões dos Vingadores, a ONU propõe o Tratado de Sokovia, um acordo para regulamentar e controlar a equipe.",
+        trailer: "ID_YOUTUBE",
+        
+        // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
+
+        duracao: "2h 20 min",
+        genero: "Ação",
+        classificacao: "+14",
+    }
+];
+
+export const series = [
+    {
+        id: "Stranger Things",
+        titulo: "Stranger Things",
+        imagem: "Imagens/strangerthings.jpg",
+        descricao: "Um grupo de crianças enfrenta forças sobrenaturais.",
+        trailer: "earch_query=stranger+things",
+        
+        // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
+
+        duracao: "5 temporadas",
+        genero: "+suspense",
+        classificacao: "+16",
+    },
+    {
+        id: "Dark",
+        titulo: "Dark",
+        imagem: "Imagens/dark.jpg",
+        descricao: "Dark é uma série focada em viagens no tempo e multiversos.",
+        trailer: "ID_YOUTUBE",
+        
+        // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
+
+        duracao: "3 temporadas",
+        genero: "ficção científica",
+        classificacao: "+16",
+    },
+    {
+        id: "Breaking Bad",
+        titulo: "Breaking Bad",
+        imagem: "Imagens/breakingbad.jpg",
+        descricao: " A série acompanha Walter White e a sua transformação de um homem comum no chefão do tráfico.",
+        trailer: "ID_YOUTUBE",
+        
+        // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
+
+        duracao: "5 temporadas",
+        genero: "drama. ação e crime",
+        classificacao: "+18",
+    },
+    {
+        id: "Peaky Blinders",
+        titulo: "Peaky Blinders",
+        imagem: "Imagens/peakyblinders.jpg",
+        descricao: "Peaky Blinders acompanha Thomas Shelby e sua gangue em Birmingham após a Primeira Guerra Mundial.",
+        trailer: "ID_YOUTUBE",
+        
+        // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
+
+        duracao: "6 temporadas",
+        genero: "ação dramática",
+        classificacao: "+18",
+    },
+    {
+        id: "Round 6",
+        titulo: "Round 6",
+        imagem: "Imagens/round6.jpg",
+        descricao: "Centenas de jogadores falidos aceitam um estranho convite para um jogo de sobrevivência. Onde um prêmio milionário aguarda.",
+        trailer: "ID_YOUTUBE",
+        
+        // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
+
+        duracao: "3 temporadas",
+        genero: "terror,comédia, ação, guerra",
+        classificacao: "+16",
+    },
+    {
+        id: "Avatar A lenda de Aang",
+        titulo: "Avatar A lenda de Aang",
+        imagem: "Imagens/avatar.jpg",
+        descricao: "Avatar: A Lenda de Aang acompanha Aang, um menino de 12 anos que acorda após cem anos congelado em um iceberg.",
+        trailer: "ID_YOUTUBE",
+        
+        // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
+
+        duracao: "3 temporadas",
+        genero: "animação",
+        classificacao: "12+",
+    },
+    {
+        id: "Simpsons",
+        titulo: "Simpsons",
+        imagem: "Imagens/simpsons.jpg",
+        descricao: "Os Simpsons é uma série de animação norte-americana que satiriza a vida da classe média americana através do cotidiano da família Simpson na cidade fictícia de Springfield.",
+        trailer: "ID_YOUTUBE",
+        
+        // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
+
+        duracao:  "38 temporadas",
+        genero: "comédia",
+        classificacao: "+12",
+    },
+    {
+        id: "prison break",
+        titulo: "prison break",
+        imagem: "Imagens/prisonb.jpg",
+        descricao: "série de suspense e drama que acompanha a genial e desesperada missão de Michael Scofield para salvar seu irmão do corredor da morte",
+        trailer: "ID_YOUTUBE",
+        
+        // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
+
+        duracao: "5 temporadas",
+        genero: "ação",
+        classificacao: "+18",
+    },
+    {
+        id: "pica pau",
+        titulo: "pica pau",
+        imagem: "Imagens/picapau.jpg",
+        descricao: "A série acompanha as loucas aventuras de um pássaro antropomórfico hiperativo, debochado e extremamente persistente, famoso por sua risada inconfundível (He-he-he-he-he!) e pelo hábito de bicar árvores e infernizar a vida de quem cruza o seu caminho.",
+        trailer: "GtDzwLiwnng",
+        
+        // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
+
+        duracao: "3 temporadas",
+        genero: "comédia",
+        classificacao: "livre",
+    },
+];
